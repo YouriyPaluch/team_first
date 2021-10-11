9be55213877df7826200c54e16bb84d438a37690
MihailExplain <unkvuzutor@gmail.com>
2021-10-11 17:16:06 +0300

    NewsController.php make function -edit -update

--- vendor/controllers/NewsController.php
+++ vendor/controllers/NewsController.php
@@ -2,33 +2,66 @@
 
 
 namespace controllers;
-use models\Store;
-use core\View;
+
 use core\Route;
+use core\View;
+use models\Store;
 
 class NewsController
 {
     /**
      * chose page index and take array from DB
      */
-public function index(){
-    $news = new Store();
-    $newsAll = $news->allNews();
-    $view = new View('index_index');
-    $view-> render($newsAll);
-}
-public function create(){
-    $view = new View('news_create');
-    $view-> render();
-}
-public function store(){
-    $newsNew = [];
-    $newsNew = $_REQUEST['title'];
-    $newsNew = $_REQUEST['text'];
-    $newsNew = $_REQUEST['author'];
-    $news = new Store();
-    $newsAll = $news->allNews();
-    $newsAll->addNews($newsNew);
-    Route::redirect();
-}
+    public function index()
+    {
+        $news = new Store();
+        $newsAll = $news->allNews();
+        $view = new View('index_index');
+        $view->render($newsAll);
+    }
+
+    /**
+     * chose page for create news
+     */
+    public function create()
+    {
+        $view = new View('news_create');
+        $view->render();
+    }
+    /**
+     * make mass  news and save in DB
+     */
+    public function store()
+    {
+        $newsNew = [];
+        $newsNew['title'] = $_REQUEST['title'];
+        $newsNew['text'] = $_REQUEST['text'];
+        $newsNew['author'] = $_REQUEST['author'];
+        $news = new Store();
+        $news->addNews($newsNew);
+        Route::redirect();
+    }
+
+    /**
+     * chose page for edit news
+     */
+    public function edit()
+    {
+        $view = new View('news_edit');
+        $view->render();
+    }
+
+    /**
+     * make mass changes news and save in DB
+     */
+    public function update()
+    {
+        $newsNew = [];
+        $newsNew['id'] = $_REQUEST['id'];
+        $newsNew['title'] = $_REQUEST['title'];
+        $newsNew['text'] = $_REQUEST['text'];
+        $news = new Store();
+        $news->saveNews($newsNew);
+        Route::redirect();
+    }
 }
