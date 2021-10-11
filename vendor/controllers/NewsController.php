7634fc93ad46ea9b6910a05c130290f77d44d1a5
MihailExplain <unkvuzutor@gmail.com>
2021-10-11 16:07:19 +0300

    Class NewsController.php function: -index; -create

--- vendor/controllers/NewsController.php
+++ vendor/controllers/NewsController.php
@@ -2,9 +2,25 @@
 
 
 namespace controllers;
-
+use models\Store;
+use core\View;
 
 class NewsController
 {
+    /**
+     * chose page index and take array from DB
+     */
+public function index(){
+    $news = new Store();
+    $newsIndex = $news->allNews();
+    $view = new View('index_index');
+    $view-> render($newsIndex);
+}
+public function create(){
+    $view = new View('news_create');
+    $view-> render();
+}
+public function store(){
 
+}
 }
