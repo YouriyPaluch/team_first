b288c8afed05fc6a4ac44041a487420bb76cd6a6
MihailExplain <unkvuzutor@gmail.com>
2021-10-11 17:14:25 +0300

    made page news_edit.php

--- vendor/views/news_edit.php
+++ vendor/views/news_edit.php
@@ -1,1 +1,12 @@
-<?php
+<form action="/NewsController/update" method="post">
+    <?php $newsMass = new Store();?>
+    <?php $news = $newsMass->getNews($_REQUEST['id']);?>
+    <input type="hidden" name="id" value="<?= $news['id']?>">
+    <label>Title
+        <input type="text" name="note" value="<?= $news['title']?>">
+    </label>
+    <label>Text
+        <input type="text" name="note" value="<?= $news['text']?>">
+    </label>
+    <input type="submit" value="Update">
+</form>
