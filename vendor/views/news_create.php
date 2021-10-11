5f989278e30a9c31c3d799da8c4809ec19e1e5eb
MihailExplain <unkvuzutor@gmail.com>
2021-10-11 16:38:38 +0300

    made page news_create.php

--- vendor/views/news_create.php
+++ vendor/views/news_create.php
@@ -1,1 +1,12 @@
-<?php
+<form action="/NewsController/store" method="post">
+    <label>Title
+        <input type="text" name="title">
+    </label>
+    <label>News
+        <input type="text" name="news">
+    </label>
+    <label>Author
+        <input type="text" name="author">
+    </label>
+    <input type="submit" value="Save">
+</form>
