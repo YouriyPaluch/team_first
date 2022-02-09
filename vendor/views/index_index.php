597235e607115b76a0de8d5a3417970147900ae4
yourii.korchevskii <yourii.korchevskii@forforce.com>
2022-02-09 16:02:48 +0200

    update project to Movie add stile

--- vendor/views/index_index.php
+++ vendor/views/index_index.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="/vendor/css/bootstrap.css">
 <a href="/"><h1>Welcome to my site</h1></a>
 <form action="/movie/index" method="GET">
     <input type="submit" value="Watch all movies">
