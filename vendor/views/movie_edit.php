5150b782fd801b30cd8be997c9d27d6aa0f41e7b
yourii.korchevskii <yourii.korchevskii@forforce.com>
2022-02-11 17:26:51 +0200

    add hash-name image

--- vendor/views/movie_edit.php
+++ vendor/views/movie_edit.php
@@ -1,33 +1,56 @@
-<form action="/movie/update" method="post" enctype="multipart/form-data" class="column">
-    <input type="hidden" name="movieId" value="<?= $movie['movieId'] ?>">
-    <div>
-        <label>Name
-            <input type="text" name="name" value="<?= $movie['name'] ?>">
-            <p class="text-error"><?= $errors['name'] ?? '' ?></p>
-        </label>
-    </div>
-    <div>
-        <label>Description
-            <textarea name="description"><?= $movie['description'] ?></textarea>
-            <p class="text-error"><?= $errors['description'] ?? '' ?></p>
-        </label>
-    </div>
-    <div>
-        <label>Data release
-            <input type="date" name="releaseDate" value="<?= $movie['releaseDate'] ?>">
-            <p class="text-error"><?= $errors['releaseDate'] ?? '' ?></p>
-        </label>
-    </div>
-    <div class="span4">Saved photo
-        <img src="<?= $movie['image'] ?>"/>
-        <input type="hidden" name="imageIfError" value="<?= $movie['image'] ?>">
-    </div>
-    <div>
-        <label>Photo
-            <input type="file" name="image" id="image">
-            <p class="text-error"><?= $errors['image'] ?? '' ?></p>
-        </label>
-    </div>
-    <input type="submit" value="Update">
-</form>
-<div id="new_photo"></div>
+<table>
+    <form action="/movie/update" method="post" enctype="multipart/form-data">
+        <input type="hidden" name="movieId" value="<?= $movie['movieId'] ?>">
+        <tr>
+            <td>
+                <label for="name">Name</label>
+            </td>
+            <td>
+                <input type="text" id="name" name="name" value="<?= $movie['name'] ?>">
+                <p class="text-error"><?= $errors['name'] ?? '' ?></p>
+            </td>
+        </tr>
+        <tr>
+
+            <td>
+                <label for="description">Description</label>
+            </td>
+            <td>
+                <textarea id="description" name="description"><?= $movie['description'] ?></textarea>
+                <p class="text-error"><?= $errors['description'] ?? '' ?></p>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label for="releaseDate">Data release</label>
+            </td>
+            <td>
+                <input type="date" id="releaseDate" name="releaseDate" value="<?= $movie['releaseDate'] ?>">
+                <p class="text-error"><?= $errors['releaseDate'] ?? '' ?></p>
+            </td>
+        </tr>
+        <tr>
+            <td>Saved photo</td>
+            <td>
+                <img class="span2" src="<?= $movie['image'] ?>"/>
+                <input type="hidden" name="imageIfError" value="<?= $movie['image'] ?>">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label>New photo</label>
+            </td>
+            <td>
+                <input type="file" name="image" id="image">
+                <div class="column" id="new_photo"></div>
+                <p class="text-error"><?= $errors['image'] ?? '' ?></p>
+            </td>
+        </tr>
+        <tr>
+            <td rowspan="2">
+                <input type="submit" value="Update">
+            </td>
+        </tr>
+    </form>
+
+</table>
